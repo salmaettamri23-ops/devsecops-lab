830cb5839a2b8797cbdfc643cd87f914e5495556
salmaettamri23-ops <salma.ettamri23@gmail.com>
2026-04-17 12:05:03 +0100

    correction mdp

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ import sqlite3
 import subprocess
 import hashlib
 import os
-
+from werkzeug.security import generate_password_hash
 app = Flask(__name__)
 
 SECRET_KEY = "dev-secret-key-12345"  # Hardcoded secret
@@ -46,8 +46,8 @@ def compute():
 @app.route("/hash", methods=["POST"])
 def hash_password():
     pwd = request.json.get("password", "admin")
-    hashed = hashlib.md5(pwd.encode()).hexdigest()
-    return {"md5": hashed}
+    hashed = generate_password_hash(pwd)
+    return {"password_hash": hashed}
 
 
 @app.route("/readfile", methods=["POST"])
